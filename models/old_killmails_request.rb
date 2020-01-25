28b0fb8e2756e482bc0de485808805baee2f787a
ced <zuger.cedric@gmail.com>
2020-01-25 14:27:07 +0100

    OldKillmailsRequest rewrote.

--- models/old_killmails_request.rb
+++ models/old_killmails_request.rb
@@ -5,28 +5,20 @@ class OldKillmailsRequest < ActiveRecord::Base
   def self.get(url)
     old_k = self.find_by_url( url )
 
-    # pp old_k
-
-    puts Time.now.gmtime - 1.hour - old_k.updated_at
-
     if old_k
       if old_k.updated_at <= Time.now - 1.hour
-
-        puts 'update'
         result = self.get_from_web(url)
         old_k.result = result
         old_k.touch # Because result can be the same even if time is out
-        old_k.save!
       else
-
         result = old_k.result
       end
     else
-
       result = self.get_from_web(url)
-      self.create!( url: url, result: result )
+      old_k = self.new( url: url, result: result )
     end
 
+    old_k.save!
     result
   end
 
