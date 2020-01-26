26adea366069d42572c2e7fbf6cedf6a44cb3049
ced <zuger.cedric@gmail.com>
2020-01-26 11:45:07 +0100

    Reactions prices monitoring fixed.

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem 'ruby-bare-esi'
+gem 'ruby-bare-esi', '0.0.3'
 gem 'activesupport'
 gem 'activerecord'
 gem 'standalone_migrations', '~> 5.2', '>= 5.2.7'
--- run/download_reactions_components_prices.sh
+++ run/download_reactions_components_prices.sh
@@ -5,4 +5,4 @@ eval "$(rbenv init -)"
 
 cd "`dirname $BASH_SOURCE`/.."
 
-bundle exec ruby download_reactions_components_prices.rb >>log/download_reactions_components_prices.log 2>>log/download_reactions_components_prices.err
+bundle exec ruby run/download_reactions_components_prices.rb >>log/download_reactions_components_prices.log 2>>log/download_reactions_components_prices.err
