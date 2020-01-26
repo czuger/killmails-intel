45e19e128843f2573c4f9a912e8cc4ecd7c81828
ced <zuger.cedric@gmail.com>
2020-01-26 10:41:09 +0100

    Script added for crontab.

---
+++ run/check_individuals_and_systems.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+cd "`dirname $BASH_SOURCE`"
+
+bundle exec ruby check_individuals_and_systems.rb >>log/check_individuals_and_systems.log 2>>log/check_individuals_and_systems.err
